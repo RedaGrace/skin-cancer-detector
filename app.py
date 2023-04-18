c403c0ae2ee69e04e2038e46445e31a17d350c05
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2023-04-18 18:38:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,55 @@
-import speech_recognition as sr
-from googletrans import Translator, constants
-from nlp_service import nlp_service
-
-
 from waitress import serve
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+from main import Predict
 import random
 import os
 import json
-from flask_cors import CORS
-#from nlp_service import nlp_service  # model class
 
+#Save images to the 'static' folder as Flask serves images from this directory
+UPLOAD_FOLDER = 'static/images/'
 
-# initialize the recognizer
-r = sr.Recognizer()
-translator = Translator()
+app = Flask(__name__, static_folder="static")
 
-app = Flask(__name__)
-CORS(app)
+#Add reference fingerprint. 
+#Cookies travel with a signature that they claim to be legit. 
+#Legitimacy here means that the signature was issued by the owner of the cookie.
+#Others cannot change this cookie as it needs the secret key. 
+#It's used as the key to encrypt the session - which can be stored in a cookie.
+#Cookies should be encrypted if they contain potentially sensitive information.
+app.secret_key = "secret key"
 
-@app.route('/', methods = ['GET'])
+#Define the upload folder to save images uploaded by the user. 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
 def home_page():
-    data_set = {'Page': 'Home', 'Message': "Let's get started and send me your text"}
-    json_dump = json.dumps(data_set)
-    return json_dump
 
-@app.route('/predict/', methods=['GET'])
-def request_page():
-    User_text = request.args.get('text') # /predict/?text=text
+    return render_template('index.html')
 
-    #audio_data = r.record(source)
-    # recognize (convert from speech to text)
-    #text_in_arabic = r.recognize_google(audio_data, language="ar-EG")
-    
-    # translate into English
-    translation = translator.translate(User_text)
-    # create instance of the model class
-    model = nlp_service()
-    # making prediction and getting response
-    response = model.get_response(translation.text)
-    # remove the audio file
-    #os.remove(file_name)
-    # send back the instructions in json format
-    data = {'source_text': User_text,
-            'translation': translation.text,
-            'firstaid_instructions': response,
-            'firstaid_instructions_in_arabic': (translator.translate(response, dest="ar")).text}
-  
-
-    data_set = json.dumps(data)
-    return data_set
+#Add Post method to the decorator to allow for form submission. 
+@app.route('/', methods=['POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            #getPrediction(filename)
+            label = getPrediction(filename)
+            flash(label)
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            flash(full_filename)
+            return redirect('/')
 
 if __name__=='__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
     #serve(app, host='0.0.0.0', port=port, debug=True)
+ 
