482509299e038c90323537ed8ced020c58ab46d1
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2023-04-18 18:45:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+
+def Predict(filename):
+    
+    #Load model
+    my_model=load_model("model/HAM10000_100epochs.h5")
+    
+    SIZE = 320 #Resize to same size as training images
+    img_path = 'static/images/'+filename
+    img = np.asarray(Image.open(img_path).resize((SIZE,SIZE)))
+    
+    img = img/255.      #Scale pixel values
+    
+    img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
+    
+    pred = my_model.predict(img) #Predict                    
+    
+    #Convert prediction to class name
+    pred_class = le.inverse_transform([np.argmax(pred)])[0]
+    print("Diagnosis is:", pred_class)
+    return pred_class
