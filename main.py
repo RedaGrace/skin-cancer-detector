e7c9887a8521e9af351d2c4a58439852b475469f
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2023-04-24 14:54:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-impoer tensoflow as tf
+impoer tensorflow as tf
 from tensorflow.keras.models import load_model
 
 class TensorflowLiteClassificationModel:
